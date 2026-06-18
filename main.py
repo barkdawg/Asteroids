a06fa7117a0e276cc61f5ea13a0c87051038674b
barkdawg <kwbarker11@gmail.com>
2026-06-18 15:27:04 -0500

    Added asteroid and asteroid field classes

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from constants import *
 from logger import log_state
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,9 +18,14 @@ def main():
     #create sprite groups and assign them to the Player class - muist do before any Playuer objects are created
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     dt = 0.0   
 
     while True:
